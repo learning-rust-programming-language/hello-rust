6449e431d4af850ca51242892f69a8d04baf1604
romelgomez <bmxandcode@gmail.com>
2023-04-24 09:04:47 -0500

    chore: add types

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,16 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    // println!("Guess the number!");
-
-    // println!("Please input your guess.");
-
-    // let mut guess = String::new();
-
-    // let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
-
-    // println!("The secret number is: {}", secret_number);
-
-    // io::stdin()
-    //     .read_line(&mut guess)
-    //     .expect("Failed to read line");
-
-    // println!("You guessed: {}", guess);
-
-    // let mut input = String::new();
-    // println!("Enter a number: ");
-    // io::stdin()
-    //     .read_line(&mut input)
-    //     .expect("Failed to read line");
-    // let input: u32 = input.trim().parse().expect("Please type a number!");
-
-    // let mut i = 1;
-    // while i <= input {
-    //     println!("{}", i);
-    //     i += 1;
-    // }
-
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess: String = String::new();
 
         io::stdin()
             .read_line(&mut guess)
